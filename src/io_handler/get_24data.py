79508261f30a94b13a94a79ef0adfe07d21b4fdd
stdmalloc <o78ltl86@anonaddy.com>
2026-01-27 13:00:08 -0800

    reorder/rename files, no rewrites
    - renamed folders so main.py can import them
    - reverted 2ab890a to add a main.py outside of the folders
    - renamed other .py files for clarity
    - reformatted comments in io_handler/get_24data.py

---
+++ src/io_handler/get_24data.py
@@ -0,0 +1,45 @@
+import asyncio
+import json
+from websockets.asyncio.client import connect as ws_connect
+
+flight_queue = [] #flights for use in data-handler, stored [{data}, {data}, ...]
+active_users = [] #list of all current users to be tracked
+
+def filter_aca(flight_data_list):
+    out = {}
+    for callsign in flight_data_list:
+        flight_info = flight_data_list[callsign] #dict of flight info WITHOUT callsign
+        if (
+            'Air Canadian' in callsign #must fly for air canada
+            and flight_info['playerName'] in active_users #must participate in our logging sys
+        ):
+            out |= {callsign:flight_info} #append dict to output
+    return out
+
+async def load_data_to_queue(uri='wss://24data.ptfs.app/wss'): #open connection, then load new data
+    async with ws_connect(uri) as websocket:
+        while True:
+            data_dump = await websocket.recv() #get json info
+            data_dict = json.loads(data_dump) #load json info to dict
+            
+            if data_dict['t'] not in ['ACFT_DATA','EVENT_ACFT_DATA']: #only use acft data (standard OR event server)
+                continue
+
+            data_dict['d'] = filter_aca(data_dict['d']) #filter to only aca planes
+            curr_time = data_dict['s'] #get current time for timestamp
+
+            for callsign in data_dict['d']: #handle json data
+
+                #NOTE: CALLSIGN IS INGAME CALLSIGN **NOT** PLAYER CHOSEN
+
+                flight_dict={ #dict of flight info
+                    **{'callsign':callsign},
+                    **data_dict['d'][callsign],
+                    **{'timestamp':curr_time},
+                }
+                flight_queue.append(flight_dict) #append to flight_queue
+
+def start_loading_acft_data(): #start loading info to flight_queue
+    asyncio.run(load_data_to_queue())
+def get_flight_queue(): #easy fn to load current flight queue
+    return flight_queue
