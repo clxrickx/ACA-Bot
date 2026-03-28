e24f1d1cfc314ad4480678a1d9033b0f2132c135
stdmalloc <o78ltl86@anonaddy.com>
2026-03-28 00:17:35 -0700

    rework get_dists.py to callable function
    - moves the distance calculation code into a callable function
    - rename flight plans to flight profiles b/c program adds extra info that isn't explicitly filed
    
    some notes to note:
    - calculate_next_dist will need to be called b/w 24data updates -> likely need to rework asyncio logic
    - acft_data_queue and flight_profiles are held in get_24data.py -> might be better to move it into main.py b/c that's where the function calls will be coordinated
    - next step likely to handle flights starting/ending, adding points (rewards), and storing point values/logs/other data

--- src/data_handler/get_dists.py
+++ src/data_handler/get_dists.py
@@ -1,43 +1,75 @@
-import time
-from src.io_handler.get_24data import flight_queue
-from src.io_handler.get_24data import dep_arr_basic as dep_arr_basic_live
+from asyncio import sleep
+from io_handler.get_24data import start_loading_acft_data
+from io_handler.get_24data import get_acft_data_queue, get_flight_profiles
 
-def slp(seconds):
-    time.sleep(seconds)
+cartesian_dist = lambda x1,y1,x2,y2: ((x2-x1)**2 + (y2-y1)**2)**0.5
+# studs_to_NM = lambda studs: studs/3307.14286 #uses 3307.14286 studs/NM, 0.56m/stud, as stated in 24data server
 
-flight_queue = flight_queue #import flight queue
+def select_fp_by_username(flight_profiles, username):
+    for flight_profile in flight_profiles:
+        if flight_profile['robloxName'] == username:
+            return flight_profile
+    raise KeyError('data_handler.get_dists.select_fp_by_username: no flight profile with username found')
 
-distsCartesian = []
-distsSpeed = []
+def calculate_next_dist():
+    #TEMP NOTE: this fn will need to be called in main.py for each iteration, preferably between fp + acft data updates
+    #TEMP NOTE: likely will need to rework the async stuff to be able to schedule this b/w 24data update calls
 
-dep_arr_basic = [] #list of [callsign, arrival, departure]
+    flight_profiles = get_flight_profiles() #stores as reference
+    acft_data_queue = get_acft_data_queue() #also stores as reference
 
-#this loop will be in effect while accurate distance calculations are being made
-while True:
-    # backup printout from the live callsign-keyed dict (added system)
-    for callsign, info in dep_arr_basic_live.items():
-        arrivalAero = info.get("arrivalAirport")
-        departureAero = info.get("departureAirport")
-        print(f"{callsign}: {departureAero} -> {arrivalAero}")
-    break
+    for new_acft_data in acft_data_queue:
+        roblox_username = new_acft_data['playerName']
+        flight_profile = select_fp_by_username(flight_profiles, roblox_username)
+        
+        if 'studsTraveled' not in flight_profile: #first hit
+            flight_profile['studsTraveled'] = 0 #initialize studs traveled value
+            flight_profile['lastPositionStuds'] = ( #initialize last position value
+                new_acft_data['position']['x'], #x,y positions are in studs
+                new_acft_data['position']['y']
+            )
+        else: #any hit after first
+            current_position = ( #get current position
+                new_acft_data['position']['x'],
+                new_acft_data['position']['y']
+            )
+            flight_profile['studsTraveled'] += cartesian_dist( #add new dist
+                *current_position, #current position
+                *flight_profile['lastPositionStuds'], #last position
+            )
+            flight_profile['lastPositionStuds'] = current_position #update last position
+        
+        acft_data_queue.remove(new_acft_data)#clear current acft data instance from queue
 
-exit()
+# distsSpeed = []
 
-#remove accurate calculations for now at least until i/o handling is completed
-while True:
-    slp(3)
-    for i in range(len(flight_data)-1):
-        acft1 = flight_data[i]
-        acft2 = flight_data[i+1]
+# dep_arr_basic = [] #list of [callsign, arrival, departure]
 
-        # Calculate Cartesian distance
-        dx = acft2['x'] - acft1['x']
-        dy = acft2['y'] - acft1['y']
-        dz = acft2['z'] - acft1['z']
-        dist_cartesian = (dx**2 + dy**2 + dz**2)**0.5
-        distsCartesian.append(dist_cartesian)
+# #this loop will be in effect while accurate distance calculations are being made
+# while True:
+#     # backup printout from the live callsign-keyed dict (added system)
+#     for callsign, info in dep_arr_basic_live.items():
+#         arrivalAero = info.get("arrivalAirport")
+#         departureAero = info.get("departureAirport")
+#         print(f"{callsign}: {departureAero} -> {arrivalAero}")
+#     break
 
-        # Calculate Speed difference
-        speed_diff = abs(acft2['speed'] - acft1['speed'])
-        distsSpeed.append(speed_diff)
-    
+# exit()
+
+# #remove accurate calculations for now at least until i/o handling is completed
+# while True:
+#     sleep(3)
+#     for i in range(len(flight_data)-1):
+#         acft1 = flight_data[i]
+#         acft2 = flight_data[i+1]
+
+#         # Calculate Cartesian distance
+#         dx = acft2['x'] - acft1['x']
+#         dy = acft2['y'] - acft1['y']
+#         dz = acft2['z'] - acft1['z']
+#         dist_cartesian = (dx**2 + dy**2 + dz**2)**0.5
+#         distsCartesian.append(dist_cartesian)
+
+#         # Calculate Speed difference
+#         speed_diff = abs(acft2['speed'] - acft1['speed'])
+#         distsSpeed.append(speed_diff)
--- src/io_handler/get_24data.py
+++ src/io_handler/get_24data.py
@@ -1,16 +1,16 @@
 import asyncio
-import json
-from websockets.asyncio.client import connect as ws_connect
+from json import loads
+from websockets.asyncio.client import connect
 
-active_users = [] #list of all current users to be tracked
+users_to_track = [] #current list of all users to be tracked
 
-flight_plans = [] #flight plans for use in data handler, stored [{data}, {data}, ...], eligible acft only
+flight_profiles = [] #flight profiles for use in data handler, stored [{data}, {data}, ...], eligible acft only
 acft_data_queue = [] #flights for use in data-handler, stored [{data}, {data}, ...], eligible acft only
 
-def is_fp_eligible(flight_plan):
+def is_fp_eligible(flight_profile):
     return (
-        'Air Canadian' in flight_plan['realcallsign'] #must fly for air canada
-        and flight_plan['robloxName'] in active_users #must participate in our logging sys
+        'Air Canadian' in flight_profile['realcallsign'] #must fly for air canada
+        and flight_profile['robloxName'] in users_to_track #must participate in our logging sys
     )
 def filter_acft_data(flight_data_list):
     out = {}
@@ -18,22 +18,22 @@ def filter_acft_data(flight_data_list):
         flight_info = flight_data_list[callsign] #dict of flight info WITHOUT callsign
         if (
             'Air Canadian' in callsign #must fly for air canada
-            and flight_info['playerName'] in active_users #must participate in our logging sys
+            and flight_info['playerName'] in users_to_track #must participate in our logging sys
         ):
             out |= {callsign:flight_info} #append dict to output
     return out # returns all aca flights as dicts where the callsign is the key
 
 async def load_data_to_queue(uri='wss://24data.ptfs.app/wss'): #open connection, then load new data
-    async with ws_connect(uri) as websocket:
+    async with connect(uri) as websocket:
         while True:
             data_dump = await websocket.recv() #get json info
-            data_dict = json.loads(data_dump) #load json info to dict
+            data_dict = loads(data_dump) #load json info to dict
 
             #NOTE 'callsign' field in data_dict is ingame callsign NOT user-chosen callsign
             
             if data_dict['t'] in ['ACFT_DATA','EVENT_ACFT_DATA']: #acft data (standard OR event server)
 
-                data_dict['d'] = filter_acft_data(data_dict['d']) #filter to only eligible aca planes
+                data_dict['d'] = filter_acft_data(data_dict['d']) #filter to only eligible aca flights
                 curr_time = data_dict['s'] #get current time for timestamp
 
                 for callsign in data_dict['d']: #handle json data
@@ -44,20 +44,28 @@ async def load_data_to_queue(uri='wss://24data.ptfs.app/wss'): #open connection,
                     }
                     acft_data_queue.append(flight_dict) #append to acft_data_queue
             
-            elif data_dict['t'] in ['FLIGHT_PLAN', 'EVENT_FLIGHT_PLAN']: #filed flight plan (standard OR event server)
+            elif data_dict['t'] in ['FLIGHT_profile', 'EVENT_FLIGHT_profile']: #filed flight profile (standard OR event server)
 
-                flight_plan = data_dict['d'] #get flight plan
+                flight_profile = data_dict['d'] #get flight profile
 
                 if (
-                    is_fp_eligible(flight_plan) #signed up AND flying aca
-                    and flight_plan not in flight_plans #avoid duplicates
+                    is_fp_eligible(flight_profile) #signed up AND flying aca
+                    and flight_profile not in flight_profiles #avoid duplicates
                 ):
-                    flight_plans.append(flight_plan)
+                    for other_fp in flight_profiles:
+                        if other_fp['robloxName'] == flight_profile['robloxName']:
+                            flight_profiles.remove(other_fp)
+                            break
+
+                    flight_profiles.append(flight_profile) #add to fp list
             
-            else: #otherwise
+            else: #if new info is NOT acft data or flight profile
                 continue #for readability's sake
 
 def start_loading_acft_data(): #start loading info to acft_data_queue
     asyncio.run(load_data_to_queue())
+
 def get_acft_data_queue(): #easy fn to load current flight queue
     return acft_data_queue
+def get_flight_profiles(): #easy fn to load current flight profiles
+    return flight_profiles
